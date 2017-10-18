851d49fcb466d9dbc12dddc4c84054c5f632ff05
dfrancese <davefrancese@gmail.com>
2017-10-18 15:56:32 -0600

    heroku try 1

--- app.js
+++ app.js
@@ -2,10 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json()
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-
-
+require('dotenv').config()
 
 app.set('view engine', 'hbs');
 
