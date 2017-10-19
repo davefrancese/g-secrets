28ae8fbe896f18e36b3ac349099983b60c531fed
dfrancese <davefrancese@gmail.com>
2017-10-19 11:12:27 -0600

    heroku w/ front page styled

--- app.js
+++ app.js
@@ -3,17 +3,19 @@ const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json()
 const app = express();
 const port = process.env.PORT || 3000;
+const path = require('path')
 
 require('dotenv').config()
 
 app.set('view engine', 'hbs');
 
-// create application/x-www-form-urlencoded parser
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req,res) => {
   res.render('index', {
-    title: 'Galvanize Secrets'
+    title: 'Galvanize Secrets',
   })
 })
 
